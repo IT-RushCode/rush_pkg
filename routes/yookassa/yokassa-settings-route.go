f3692af2174061a8cae0b63845afa8626e6fe241
Coffe-Lake <felixkot00@gmail.com>
2024-08-23 04:14:07 +0500

    Update rush_pkg

--- routes/yookassa/yokassa-settings-route.go
+++ routes/yookassa/yokassa-settings-route.go
@@ -8,7 +8,8 @@ import (
 func RUN_YOOKASSA_SETTINGS_ROUTES(api fiber.Router, ctrl *controllers.Controllers) {
 	ykSetting := api.Group("yookassa-settings")
 
-	ykSetting.Get("/by-point", ctrl.YookassasettingController.FindYooKassaSettingByPointID).Name("view:yookassa_settings")
+	ykSetting.Get("/by-point", ctrl.YookassasettingController.FindYooKassaSettingByPointID).Name("view:yookassa_setting_by_point")
+
 	ykSetting.Get("/:id", ctrl.YookassasettingController.FindYooKassaSettingByID).Name("view:yookassa_setting_by_id")
 	ykSetting.Post("/", ctrl.YookassasettingController.CreateYooKassaSetting).Name("create:yookassa_setting")
 	ykSetting.Put("/:id", ctrl.YookassasettingController.UpdateYooKassaSetting).Name("update:yookassa_setting")

d5d807e6cb7c70d09078ff74f318109b7f94058b
FaraDev <felixkot00@gmail.com>
2025-01-15 01:47:05 +0500

    Update rush_pkg

--- utils/paginate.go
+++ utils/paginate.go
@@ -33,3 +33,37 @@ func Paginate(offset, limit uint) func(db *gorm.DB) *gorm.DB {
 		return db.Offset(offset).Limit(pageSize)
 	}
 }
+
+// SQLPaginate формирует строку LIMIT и OFFSET для SQL-запроса
+func SQLPaginate(offset, limit uint) (string, []interface{}) {
+	// Значения по умолчанию
+	if limit == 0 {
+		limit = 20
+	}
+
+	if offset == 0 {
+		offset = 1
+	}
+
+	page := int(offset)
+	if page <= 0 {
+		page = 1
+	}
+
+	pageSize := int(limit)
+
+	switch {
+	case pageSize > 100:
+		pageSize = 100
+	case pageSize <= 0:
+		pageSize = 20
+	}
+
+	sqlOffset := (page - 1) * pageSize
+
+	// Формируем SQL-строку и параметры
+	sqlClause := "LIMIT ? OFFSET ?"
+	args := []interface{}{pageSize, sqlOffset}
+
+	return sqlClause, args
+}

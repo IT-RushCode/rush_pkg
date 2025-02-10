0ad2f488dda73c94e1d6e1ff5f462d0565f8e10e
FaraDev <felixkot00@gmail.com>
2025-02-10 23:30:15 +0500

    Update rush_pkg

--- config/app-config.go
+++ config/app-config.go
@@ -4,6 +4,7 @@ package config
 type AppConfig struct {
 	ENV           string `mapstructure:"ENV"`
 	DEBUG         bool   `mapstructure:"DEBUG"`
+	DB_DEBUG      bool   `mapstructure:"DB_DEBUG"`
 	NAME          string `mapstructure:"NAME"`
 	HOST          string `mapstructure:"HOST"`
 	PORT          string `mapstructure:"PORT"`
